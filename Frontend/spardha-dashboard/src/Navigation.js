f1959ee37bd8327d7ed280dfc4013e1f11e25417
Atharvat <tagalpallewaratharva@gmail.com>
2022-03-15 19:46:45 +0530

    style for bottom nav

--- Frontend/spardha-dashboard/src/Navigation.js
+++ Frontend/spardha-dashboard/src/Navigation.js
@@ -1,45 +1,35 @@
 import React from 'react';
-import { Nav, NavItem} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faHome, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faArrowTrendUp, faTrophy, faCalendarDay } from '@fortawesome/free-solid-svg-icons';
 
 const tabs = [{
   route: "/standings",
-  icon: faHome,
+  icon: faArrowTrendUp,
   label: "Standings"
 },{
-  route: "/results",
-  icon: faSearch,
-  label: "Results"
-},{
   route: "/schedule",
-  icon: faUserCircle,
+  icon: faCalendarDay,
   label: "Schedule"
-}]
+},{
+    route: "/results",
+    icon: faTrophy,
+    label: " Results "
+  }]
 
 const Navigation = (props) => {
     return (
-        <div>
-            <nav className="navbar fixed-bottom bottom-tab-nav" role="navigation">
-                <Nav className="w-100">
-                    <div className=" d-flex flex-row justify-content-around w-100">
-                        {
-                        tabs.map((tab, index) =>(
-                            <NavItem key={`tab-${index}`}>
-                            <NavLink to={tab.route} className="nav-link bottom-nav-link" activeClassName="active">
-                                <div className="row d-flex flex-column justify-content-center align-items-center">
-                                <FontAwesomeIcon size="lg" icon={tab.icon}/>
-                                <div className="bottom-tab-label">{tab.label}</div>
-                                </div>
-                            </NavLink>
-                            </NavItem>
-                        ))
-                        }
+        <div className="fixed-bottom w-100 bottom_navbar pt-1">
+            {tabs.map((tab, i) =>(
+                <NavLink to={tab.route} className="d-flex flex-column justify-content-center align-items-center p-1 nav_link">
+                    <div className="icon_bg">
+                        <FontAwesomeIcon size="s" icon={tab.icon} className="fa_icon"/>
                     </div>
-                </Nav>
-            </nav>
+                    <div>{tab.label}</div>
+                </NavLink>
+            ))}
         </div>
+
     )
 };
 
