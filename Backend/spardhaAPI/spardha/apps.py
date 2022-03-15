c27777fc53540a65534013a5c6fd666744ddcfc2
Atharvat <tagalpallewaratharva@gmail.com>
2022-03-15 12:12:58 +0530

    created bottom nav

--- Backend/spardhaAPI/spardha/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpardhaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'spardha'
--- Backend/spardhaAPI/spardha/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Sport
-
-
-class SportSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Sport
-        fields = ['name', 'hostels', 'final_points']
