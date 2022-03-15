b01d7a6f21e772b4510450872be7561000771f94
Atharvat <tagalpallewaratharva@gmail.com>
2022-03-15 21:48:32 +0530

    added table on standings page

--- Frontend/spardha-dashboard/src/pages/Standings.js
+++ Frontend/spardha-dashboard/src/pages/Standings.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import StandingsItem from './components/StandingsItem';
+
+const data = [{
+    name: "Brahmaputra",
+    points: 800,
+  },{
+    name: "Lohit",
+    points: 700,
+  },{
+    name: "Siang",
+    points: 700,
+  },{
+    name: "Manas",
+    points: 600,
+  },{
+    name: "Kapili",
+    points: 500,
+  },{
+    name: "Disang",
+    points: 400,
+  },]
 
 const Standings = (props) => {
 return (
-    <div>
-      Standings
+    <div className="p-4">
+      <div className="standings_h2">
+          TABULAR FORM
+      </div>
+      {data.map((hostel, i) =>(
+            <StandingsItem Name={hostel.name} Points={hostel.points}/>
+      ))}
     </div>
   )
 };
