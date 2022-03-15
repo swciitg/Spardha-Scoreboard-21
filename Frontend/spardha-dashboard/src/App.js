5e3fca287d2cd30c0fe8117fa9adba741747d51f
Atharvat <tagalpallewaratharva@gmail.com>
2022-03-15 23:42:29 +0530

    completed standings page  UI (without graph)

--- Frontend/spardha-dashboard/src/App.js
+++ Frontend/spardha-dashboard/src/App.js
@@ -5,11 +5,13 @@ import Standings from './pages/Standings';
 import Results from './pages/Results';
 import Schedule from './pages/Schedule';
 import Navigation from './Navigation';
+import Header from './Header';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Navigation />
         <Switch>
           <Route path="/standings" component={Standings} />
--- Frontend/spardha-dashboard/src/pages/Standings.js
+++ Frontend/spardha-dashboard/src/pages/Standings.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import StandingsItem from './components/StandingsItem';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 const data = [{
     name: "Brahmaputra",
     points: 800,
@@ -24,12 +25,18 @@ const data = [{
 const Standings = (props) => {
 return (
     <div className="p-4">
-      <div className="standings_h2">
-          TABULAR FORM
-      </div>
-      {data.map((hostel, i) =>(
-            <StandingsItem Name={hostel.name} Points={hostel.points}/>
-      ))}
+        <div className="standings_header d-flex flex-row align-items-center">
+            <div className="standings_text">STANDINGS</div>
+            <div className="standings_dropdown_text">Overall</div>
+            <FontAwesomeIcon icon={faCaretDown} className="standings_dropdown_icon" size="l"/>
+        </div>
+        <div className="w-100 black_line" />
+        <div className="standings_h2">
+            TABULAR FORM
+        </div>
+        {data.map((hostel, i) =>(
+            <StandingsItem Name={hostel.name} Points={hostel.points} Index={i}/>
+        ))}
     </div>
   )
 };
