90170ebcab5144de201f69140720995bf052b859
shiv <xqbptt@gmail.com>
2022-03-17 23:32:28 +0530

    modified urls

--- Backend/spardhaAPI/spardhaAPI/urls.py
+++ Backend/spardhaAPI/spardhaAPI/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('spardhaApi/admin/', admin.site.urls),
     path('spardhaApi/',include('spardha.urls')),
 ]
