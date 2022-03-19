6f2c98a5216d51ff2d41c6e6bf7f5fe7e81891d7
shiv <xqbptt@gmail.com>
2022-03-19 10:54:44 +0530

    Standings completed

--- Frontend/spardha-dashboard/src/pages/Standings.js
+++ Frontend/spardha-dashboard/src/pages/Standings.js
@@ -1,76 +1,130 @@
-import React from 'react';
 import StandingsItem from './components/StandingsItem';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'react-datepicker/dist/react-datepicker.css';
 const data = [{
-    name: "Brahmaputra",
-    points: 800,
-  },{
-    name: "Lohit",
-    points: 700,
-  },{
-    name: "Siang",
-    points: 700,
-  },{
-    name: "Manas",
-    points: 600,
-  },{
-    name: "Kapili",
-    points: 500,
-  },{
-    name: "Disang",
-    points: 400,
-  },];
+  name: "Brahmaputra",
+  points: 800,
+}, {
+  name: "Lohit",
+  points: 700,
+}, {
+  name: "Siang",
+  points: 700,
+}, {
+  name: "Manas",
+  points: 600,
+}, {
+  name: "Kapili",
+  points: 500,
+}, {
+  name: "Disang",
+  points: 400,
+},];
 
-  const sport = [
-    {
-      name: 'Cricket',
-    },
-    {
-      name: 'Basketball',
-    },
-    {
-      name: 'Volleyball',
-    },
-    {
-      name: 'Badminton',
-    },
-    {
-      name: 'Football',
-    },
-    {
-      name: 'Hockey',
-    },
-    {
-      name: 'Chess',
-    },
-    {
-      name: 'Squash',
-    },
-  ];
-  
+const sport = [
+  {
+    name: 'Cricket',
+  },
+  {
+    name: 'Basketball',
+  },
+  {
+    name: 'Volleyball',
+  },
+  {
+    name: 'Badminton',
+  },
+  {
+    name: 'Football',
+  },
+  {
+    name: 'Hockey',
+  },
+  {
+    name: 'Chess',
+  },
+  {
+    name: 'Squash',
+  },
+];
+
 
 const Standings = (props) => {
-return (
+  const baseApiURL = 'https://swc.iitg.ac.in/spardhaApi/';
+  const [hostelApiURL, setHostelApiURL] = useState(baseApiURL + 'hostels/');
+  const [standingsApiURL, setSportApiURL] = useState(baseApiURL + 'standings/');
+  const [sportApiURL, setSportsApiURL] = useState(baseApiURL + 'sports/');
+  const [hostels, setHostels] = useState([]);
+  const [sports, setSports] = useState([]);
+  const [standings, setStandings] = useState([]);
+  const [selectedSport, setSelectedSport] = useState("-1");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(hostelApiURL).then((response) => {
+      console.log(response.data);
+      setHostels(response.data);
+      setLoading(false);
+    })
+  }, [hostelApiURL]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(sportApiURL).then((response) => {
+      console.log(response.data);
+      setSports(response.data);
+      setLoading(false);
+    })
+  }, [sportApiURL]);
+  useEffect(() => {
+    console.log("triggered")
+    setLoading(true);
+    console.log(selectedSport)
+    if (selectedSport !== "-1") {
+      axios.get(standingsApiURL + "?sport=" + selectedSport).then((response) => {
+        console.log(response.data);
+        setStandings(response.data);
+        setLoading(false);
+      })
+    }
+    setLoading(false)
+  }, [standingsApiURL, selectedSport]);
+
+  const sportHandler = (event) => {
+    console.log(event.target.value)
+    setSelectedSport(event.target.value)
+  }
+
+  return (
     <div className="p-4">
-        <div className="standings_header d-flex flex-row align-items-center">
-            <div className="standings_text">STANDINGS</div>
-            <select className="standings_dropdown" name='' id=''>
-                <option className="standings_dropdown_text">Overall</option>
-                {sport.map((hostel, i) => (
-                  <option value={i} className="standings_dropdown_text">{hostel.name}</option>
-                ))}
-            </select>
-           {/*  <div className="standings_dropdown_text">Overall</div>
+      <div className="standings_header d-flex flex-row align-items-center">
+        <div className="standings_text">STANDINGS</div>
+        <select onChange={sportHandler} className="standings_dropdown" name='' id=''>
+          <option value={-1} className="standings_dropdown_text">Overall</option>
+          {sports && sports.map((sport, i) => (
+            <option value={sport.id} className="standings_dropdown_text">{sport.name}</option>
+          ))}
+        </select>
+        {/*  <div className="standings_dropdown_text">Overall</div>
             <FontAwesomeIcon icon={faCaretDown} className="standings_dropdown_icon" size="l"/> */}
-        </div>
-        <div className="w-100 black_line" />
-        <div className="standings_h2">
-            TABULAR FORM
-        </div>
-        {data.map((hostel, i) =>(
+      </div>
+      <div className="w-100 black_line" />
+      <div className="standings_h2">
+        TABULAR FORM
+      </div>
+      {loading ? (<p>Loading...</p>) : (selectedSport === "-1" ? (hostels.map((hostel, i) => (
+        <StandingsItem Name={hostel.name} Points={hostel.overall_points} Index={i} />
+      ))) : (standings.map((hostel, i) => (
+        <StandingsItem Name={hostel.hostel} Points={hostel.points} Index={i} />
+      ))))}
+
+      {/* {data.map((hostel, i) =>(
             <StandingsItem Name={hostel.name} Points={hostel.points} Index={i}/>
-        ))}
+        ))} */}
     </div>
   )
 };
