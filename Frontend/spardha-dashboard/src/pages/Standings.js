bdea9f6a2317376c93dd9a3b7a4dd8a3a04e39c4
Atharvat <tagalpallewaratharva@gmail.com>
2022-03-19 09:53:48 +0530

    api calls in standings

--- Frontend/spardha-dashboard/src/pages/Standings.js
+++ Frontend/spardha-dashboard/src/pages/Standings.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import StandingsItem from './components/StandingsItem';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 const data = [{
     name: "Brahmaputra",
     points: 800,
@@ -22,7 +21,7 @@ const data = [{
     points: 400,
   },];
 
-  const sport = [
+/*   const sport = [
     {
       name: 'Cricket',
     },
@@ -47,18 +46,47 @@ const data = [{
     {
       name: 'Squash',
     },
-  ];
+  ]; */
   
 
 const Standings = (props) => {
+  const baseApiURL = 'http://localhost:8000/spardhaApi/';
+  const [hostelApiURL, setHostelApiURL] = useState(baseApiURL+'hostels/');
+  const [sportApiURL, setSportApiURL]  = useState(baseApiURL+'sports/');
+  const [standingsApiURL, setStandingsApiURL] = useState(baseApiURL+'standings/');
+  const [hostels, setHostels] = useState([]);
+  const [sports, setSports] = useState([]);
+  const [standings, setStandings] = useState([]);
+  const [selectedSport, setSelectedSport] = useState('');
+  const [selectedStandings, setSelectedStandings] = useState([]);
+
+  useEffect(() => {
+    axios.get(hostelApiURL).then((response) =>{
+      console.log(response.data);
+      setHostels(response.data);
+    })
+  }, [hostelApiURL]);
+  useEffect(() => {
+    axios.get(sportApiURL).then((response) =>{
+      console.log(response.data);
+      setSports(response.data);
+    })
+  }, [sportApiURL]);
+  useEffect(() => {
+    axios.get(standingsApiURL).then((response) =>{
+      console.log(response.data);
+      setStandings(response.data);
+    })
+  }, [standingsApiURL]);
+
 return (
     <div className="p-4">
         <div className="standings_header d-flex flex-row align-items-center">
             <div className="standings_text">STANDINGS</div>
             <select className="standings_dropdown" name='' id=''>
                 <option className="standings_dropdown_text">Overall</option>
-                {sport.map((hostel, i) => (
-                  <option value={i} className="standings_dropdown_text">{hostel.name}</option>
+                {sports.map((sport, i) => (
+                  <option value={i} className="standings_dropdown_text">{sport.name}</option>
                 ))}
             </select>
            {/*  <div className="standings_dropdown_text">Overall</div>
@@ -68,7 +96,7 @@ return (
         <div className="standings_h2">
             TABULAR FORM
         </div>
-        {data.map((hostel, i) =>(
+        {standings.map((hostel, i) =>(
             <StandingsItem Name={hostel.name} Points={hostel.points} Index={i}/>
         ))}
     </div>
