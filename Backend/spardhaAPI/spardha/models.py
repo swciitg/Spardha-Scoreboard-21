24a5664b0742c9ce53864d1d7ec56a71e3cb7767
shiv <xqbptt@gmail.com>
2022-03-20 17:48:49 +0530

    added score updation

--- Backend/spardhaAPI/spardha/models.py
+++ Backend/spardhaAPI/spardha/models.py
@@ -52,12 +52,21 @@ class Match_all(models.Model):
     date = models.DateField(null=True)
     time = models.TimeField(null=True)
     round = models.ForeignKey("Stage",  on_delete=models.CASCADE,null= True)
+    scores = models.TextField(null=True,blank=True)
 
     @property
     def name(self):
         return self.sport.name + " - " + self.round.stage
     def __str__(self):
         return self.sport.name + " - " + self.round.stage
+
+    # def save(self, *args, **kwargs):
+    #     super(Match_all, self).save(*args, **kwargs)
+    #     print(self.hostels.all())
+    #     for hostel in self.hostels.all():
+    #         score = Score.objects.create(hostel = hostel,match=self,score=0)
+    #         print(score)
+    #         score.save()
 
 class Point(models.Model):
     hostel = models.ForeignKey("Hostel", related_name="hostels", on_delete=models.CASCADE,null= True)
@@ -75,5 +84,8 @@ class Score(models.Model):
     match = models.ForeignKey("Match_all",  on_delete=models.CASCADE,null= True)
     score = models.IntegerField()
 
+    class Meta:
+        unique_together = ('hostel', 'match',)
+
     def __str__(self):
-        return self.sport.name+" ( "+self.hostel.name+" ) "
+        return self.match.name+" ( "+self.hostel.name+" ) "
