59ae8b4926e5051746841c3c4d1cd3c939612f10
xqbptt <xqbptt@gmail.com>
2022-03-24 16:48:19 +0000

    Deployment changes

--- Backend/spardhaAPI/spardhaAPI/settings.py
+++ Backend/spardhaAPI/spardhaAPI/settings.py
@@ -127,12 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/spardhaApi/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/spardhaApi/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
